3d88d96e19826ab7a4a9ebbc89568b960b988289
Nico <salvador.nico@gmail.com>
2017-10-05 17:32:17 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+	#app
+		test
+</template>
+
+<script>
+import Test from "./components/Test"
+
+export default {
+	name: 'app',
+	components: {
+		Test
+	}
+}
+</script>
+
+<style lang="stylus">
+#app
+	font-family 'Avenir', Helvetica, Arial, sans-serif
+	-webkit-font-smoothing antialiased
+	-moz-osx-font-smoothing grayscale
+	text-align center
+	color #2c3e50
+	margin-top 60px
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+.test
+	input(type="text" v-model="title")
+	h1 {{ title }}
+	p(v-if="showName") {{ user.firstName }} {{ user.lastName }}
+	p(v-else) Nobody
+	ul
+		li(v-for="item in items") {{ item.title }}
+
+	input(type="text" v-model="greeting")
+	br
+	button(v-on:click="greet(greeting)") Say Greeting
+	br
+	br
+	input(type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit")
+</template>
+
+<script>
+export default {
+	name: "Test",
+	data() {
+		return {
+			title: "Hello World",
+			user: {
+				firstName: "John",
+				lastName: "Doe"
+			},
+			showName: true,
+			items: [
+				{ title: "Item One" },
+				{ title: "Item Two" },
+				{ title: "Item Three" },
+			],
+			greeting: ""
+		}
+	},
+	methods: {
+		greet: (greeting) => {
+			alert(`Hello ${greeting}`)
+		},
+		pressKey: (e) => {
+			console.log(e.target.value)
+		},
+		enterHit: () => {
+			console.log("Ouch")
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
